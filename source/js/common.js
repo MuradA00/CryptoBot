63ab326c994373414c5b54618576956d5acde61a
Murad <abishlimurad@gmail.com>
2022-09-07 18:26:27 +0200

    last fixes

--- source/js/common.js
+++ source/js/common.js
@@ -8,6 +8,8 @@ const selectorBtns = document.querySelectorAll('.selector-row__item'),
       filterModal = document.getElementById('filter-modal'),
       filterTrigger = document.getElementById('filterTrigger'),
       filterCloseIcon = document.getElementById('filterClose')
+
+
 selectorBtns.forEach(selectorBtn => {
   selectorBtn.addEventListener('click', () => {
     selectorBtn.classList.toggle('_selector-active')
@@ -43,24 +45,23 @@ const progressButton = document.querySelector('.progress__button'),
       progressNumber = document.querySelector('.progress__counter');
 
 
-initialButton.innerHTML = 'Start';
-progressNumber.innerHTML = '0%';
+
 
 initialButton.addEventListener('click', () => {
   progressActive();
-  if (progressBar.classList.contains('--startBar')) {
-    progressButton.addEventListener('click', () => {
-      progressBar.classList.remove('--startBar');
-      progressNumber.innerHTML = '0%';
-      initialButton.innerHTML = 'Start';
-    })
-  } else {
-    progressActive();
-  }
+  // if (progressBar.classList.contains('--startBar')) {
+  //   progressButton.addEventListener('click', () => {
+  //     progressBar.classList.remove('--startBar');
+  //     progressNumber.innerHTML = '0%';
+  //     initialButton.innerHTML = 'Start';
+  //   })
+  // } else {
+  //   progressActive();
+  // }
 });
 
 function progressActive() {
-  progressBar.classList.add('--startBar');
+  progressBar.classList.toggle('--startBar');
   progressNumber.innerHTML = '23%';
   initialButton.innerHTML = 'Stop';
 }
@@ -73,5 +74,52 @@ panel.addEventListener('click', function(e) {
   Array.from(selectorItems).forEach(item => {
   	item.classList.remove('_active-selector')
   })
-  target.classList.add('_active-selector')
+  target.classList.add('_active-selector');
+  if (wtfTrigger.classList.contains('_active-selector')) {
+    openWtfModal();
+  } else {
+    if (stepsTrigger.classList.contains('_active-selector')) {
+      testingTrigger.removeEventListener;
+      testingTrigger.addEventListener('click', () => {
+        stepsModal.classList.add('--show-modal');
+        closeStepsIcon.addEventListener('click', () => {
+          stepsModal.classList.remove('--show-modal')
+        })
+      })
+    }
+  }
+
 })
+
+
+
+function openWtfModal() {
+  testingTrigger.addEventListener('click', () => {
+    wtfModal.classList.add('--show-modal');
+    closeWtfIcon.addEventListener('click', () => {
+      wtfModal.classList.remove('--show-modal')
+    })
+  })
+}
+
+const stepsTrigger = document.querySelector('._stepsTrigger'),
+      wtfTrigger = document.querySelector('._wtfTrigger'),
+      testingTrigger = document.getElementById('testingTrigger'),
+      wtfModal = document.getElementById('wtfModal'),
+      stepsModal = document.getElementById('stepsModal'),
+      closeStepsIcon = document.getElementById('stepsClose'),
+      closeWtfIcon = document.getElementById('wtfClose')
+
+// function openHiddenModal(currentTrigger, currentModal, testingTrigger, currentCloseIcon) {
+//   if (currentTrigger == true) {
+//     testingTrigger.addEventListener('click', () => {
+//       currentModal.classList.add('--show-modal');
+//     }, {once: true});
+//     currentCloseIcon.addEventListener('click', () => {
+//       currentModal.classList.remove('--show-modal')
+//     });
+//   }
+// }
+
+
+// openHiddenModal();
