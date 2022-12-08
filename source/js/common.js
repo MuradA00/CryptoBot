88dca85416a38d23fe8bc5ed6f13ea9930870dd3
City7g <City7gor@gmail.com>
2022-12-08 16:18:57 +0200

    Add echarts

--- source/js/common.js
+++ source/js/common.js
@@ -1485,3 +1485,129 @@ areaSeries.setData([
     value: 1312550.4040613947,
   },
 ]);
+
+
+var myChart = echarts.init(document.getElementById('main'));
+
+var data1 = [];
+// var data2 = [];
+// var data3 = [];
+var random = function (max) {
+  return (Math.random() * max).toFixed(3);
+};
+var randomM = function (max) {
+  return (Math.random() * max - max / 2).toFixed(3);
+};
+for (var i = 0; i < 50; i++) {
+  data1.push([random(15), randomM(10), 0.2]);
+  // data2.push([random(10), randomM(10), 0.2]);
+  // data3.push([random(15), randomM(10), 0.2]);
+}
+option = {
+  animation: false,
+  // legend: {
+  //   data: ['scatter', 'scatter2', 'scatter3']
+  // },
+  tooltip: {},
+  xAxis: {
+    // min: 1
+    // type: 'value',
+    // min: 'dataMin',
+    // max: 'dataMax',
+    // splitLine: {
+      //   show: true
+      // }
+  },
+  yAxis: {
+    // min: 1
+    // type: 'value',
+    // min: 'dataMin',
+    // max: 'dataMax',
+    // splitLine: {
+    //   show: true
+    // }
+  },
+  dataZoom: [
+    // {
+    //   type: 'slider',
+    //   show: true,
+    //   xAxisIndex: [0],
+    //   start: 1,
+    //   end: 35
+    // },
+    // {
+    //   type: 'slider',
+    //   show: true,
+    //   yAxisIndex: [0],
+    //   left: '93%',
+    //   start: 29,
+    //   end: 36
+    // },
+    {
+      type: 'inside',
+      xAxisIndex: [0],
+      rangeMode: 2
+      // end: 2
+      // start: 1,
+      // end: 35
+    },
+    {
+      type: 'inside',
+      yAxisIndex: [0],
+      rangeMode: 2
+      // start: 1,
+      // end: 36
+    }
+  ],
+  series: [
+    {
+      name: 'scatter',
+      type: 'scatter',
+      itemStyle: {
+        normal: {
+          opacity: 0.8
+        }
+      },
+      // colorBy: '#5ed5a8',
+      itemStyle: {
+        color: '#5ed5a8'
+      },
+      symbolSize: function (val) {
+        return val[2] * 40;
+      },
+      data: data1
+    },
+    // {
+    //   name: 'scatter2',
+    //   type: 'scatter',
+    //   itemStyle: {
+    //     normal: {
+    //       opacity: 0.8
+    //     }
+    //   },
+    //   symbolSize: function (val) {
+    //     return val[2] * 40;
+    //   },
+    //   data: data2
+    // },
+    // {
+    //   name: 'scatter3',
+    //   type: 'scatter',
+    //   itemStyle: {
+    //     normal: {
+    //       opacity: 0.8
+    //     }
+    //   },
+    //   symbolSize: function (val) {
+    //     return val[2] * 40;
+    //   },
+    //   data: data3
+    // }
+  ]
+};
+
+myChart.setOption(option);
+
+myChart.on('click', function(params) {
+  alert(`Елемент под номером - ${params.dataIndex}`)
+});
